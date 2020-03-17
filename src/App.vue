a5c52c1a68e6d1f69282a9f1bfbfadf193eca03a
whycck <whycck@163.com>
2020-03-17 17:16:09 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Button type="primary" @click="sayLove">你好</Button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import { Button } from 'vant'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Button
+  },
+  methods: {
+    sayLove () {
+      console.log('你好, Vant')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
